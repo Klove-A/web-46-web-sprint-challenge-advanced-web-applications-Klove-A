d8794a585095af90da43570670dbe292a66a698c
Klove Adams <kalebsxott@gmail.com>
2021-09-17 19:03:21 -0400

    wrok

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -10,5 +10,15 @@ test("Renders without errors", () => {
 
 test("Renders appropriate number of colors passed in through mock", async () => {
   //Keep in mind that our service is called on mount for this component.
-  render(<BubblePage />);
+  const { rerender } = render(<BubblePage />);
+
+  const colors = await screen.findAllByTestId(/color/i);
+
+  expect(colors).not.toBeInTheDocument;
+
+  // rerender(<BubblePage />);
+
+  // const colors = await screen.findAllByTestId(/color/i);
+
+  // expect(colors).toHaveLength(11);
 });
